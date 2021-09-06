966f9d05eea88db270ac3944faecc6abd44b2d57
Juan Carlos Diaz <judiazp2p@gmail.com>
2021-09-06 17:53:08 -0300

    renderizando datos de la api

--- src/components/pages/home/index.jsx
+++ src/components/pages/home/index.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Header } from '../../molecules/header'
 import { AddTeam } from '../../molecules/forms/AddTeam'
 import { Team } from '../../organisms/team'
 
 export const Home = () => {
+
+    const initialValueRecords = JSON.parse(localStorage.getItem('listTeam')) || []
+    const [listTeams, setListTeams] = useState(initialValueRecords)
+
+    useEffect(() => {
+        localStorage.setItem('listTeam', JSON.stringify(listTeams))
+    }, [listTeams])
+
+    console.log(listTeams);
+
     return (
         <div>
             <Header logueado={true} />
@@ -11,8 +21,13 @@ export const Home = () => {
                 className="container-fluid"
                 style={{minHeight: "calc(100vh - 70px)"}}    
             >
-                <AddTeam />
-                <Team />
+                <AddTeam 
+                    setListTeams={setListTeams} 
+                    listTeams={listTeams}
+                />
+                <Team 
+                    listTeams={listTeams}
+                />
             </div>
         </div>
     )
